25014e09044b814537e6aac6d5a0dc570e26cf44
Данила Павлов <d.pavlov@wptt.ru>
2024-03-05 18:53:11 +0500

    two

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -5,12 +5,30 @@ export function menuToggle () {
         document.querySelector('.tests__menu').classList.toggle('active')
     }
     const options = document.querySelectorAll('li')
+    const btnStart = document.querySelector('.start')
+    const btnCancel = document.querySelector('.cancel')
+    const fremeTestStart = document.querySelector('.tests__test-start')
+    const fremeTest = document.querySelector('.tests__test')
 
     options.forEach(elem => {
         elem.addEventListener('click', ()=> {
-            document.querySelector('.tests__test').classList.add('disabled')
-            document.querySelector('.tests__test-start').classList.add('active')
-            elem.classList.add('ative')
+            for(let i = 0; i < options.length; i++){
+                options[i].classList.remove('active')
+            }
+            document.querySelector('.tests__text').classList.add('disabled')
+            fremeTestStart.classList.add('active')
+            fremeTest.classList.remove('active')
+            elem.classList.add('active')
+            btnStart.onclick = () => {
+                fremeTestStart.classList.remove('active')
+                fremeTest.classList.add('active')
+            }
+            btnCancel.onclick = () => {
+                fremeTestStart.classList.remove('active')
+                fremeTest.classList.remove('active')
+                elem.classList.remove('active')
+                document.querySelector('.tests__text').classList.remove('disabled')
+            }
         })
     })
 }
